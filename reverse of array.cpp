65692748fc605d31e156699730653d57bfa76f58
Vijay choudhary <82598434+vijaychoudhary33@users.noreply.github.com>
2021-09-24 22:50:00 +0530

    best approach  for reverse of a array

---
+++ reverse of array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int n=5;
+      int a[n]={1,2,3,4,5}
+      for( int i=0;i<n/2;i++){
+         swap(a[i],a[n-1-i]);
+        }
+      for( int i=0;i<n;i++){
+        cout<<arr[i];
+      }
+         return 0;
+  }           
+       
